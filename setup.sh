88d6d5e6c0e05cf3e14b096c453d9ac1fa6e54e0
k1rh4 <k1rh4.lee@gmail.com>
2020-10-02 11:50:02 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo pip3 install selenium
+sudo pip3 install bs4
+sudo apt-get install python-bs4
+echo "Get chrome version via insert string 'chrome://version' into url "
+echo "window 32bit driver support to 64bit."
+
