daefc33b727cf55082191ecc8c47de2d3c319492
k1rh4 <k1rh4.lee@gmail.com>
2020-10-02 11:51:14 +0900

    Init setting get google.com

--- start.py
+++ start.py
@@ -3,4 +3,5 @@ from selenium import webdriver
 driver_path = "/mnt/c/Users/k1rha/Desktop/Tools/chromedriver.exe"
 driver = webdriver.Chrome(driver_path)
 driver.implicitly_wait(3)
+driver.get('https://google.com')
 
